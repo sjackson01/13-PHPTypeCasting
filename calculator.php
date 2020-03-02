525e15976b8594c8a93cb692c46fce4d87cc4b6c
sjackson01 <stevenpjackson0@gmail.com>
2020-03-02 15:24:40 -0600

    Inital Commit

---
+++ calculator.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Widget Cost Calculator</title>
+</head>
+<body>
+<?php # Script 13.2 - calculator.php  // This script calculates an order total based upon three form values.
+
+// Check if the form has been submitted:
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+     
+    // Calculate the total:
+    $total = $quantity * $price;
+    $total += $total * ($tax/100);
+
+    // Print the result:
+    echo '<p>The total cost of purchasing ' . $quantity . ' widget(s) at $' . 
+         number_format($price, 2) . ' each, plus tax, is $' . number_format($total, 2) . '.</p>';
+
+    } else { // Invalid submitted values.
+    echo '<p style="font-weight: bold; color: #C00">Please enter a valid quantity, price,
+        and tax rate.</p>';
+    } // End of main isset() IF.
+
+// Leave the PHP section and create the HTML form.
+?>
+<h2>Widget Cost Calculator</h2>
+<form action="calculator.php" method="post">
+    <p>Quantity: <input type="number" name="quantity" step="1" min="1" value="<?php if
+    (isset($quantity)) echo $quantity; ?>"></p>
+    <p>Price: <input type="number" name="price" step=".01" min="0.01" value="<?php if
+    (isset($price)) echo $price; ?>"></p>
+    <p>Tax (%): <input type="text" name="tax" step=".01" min="0.01" value="<?php if
+    (isset($tax)) echo $tax; ?>"></p>
+    <p><input type="submit" name="submit" value="Calculate!"></p>
+</form>
+</body>
+</html>
